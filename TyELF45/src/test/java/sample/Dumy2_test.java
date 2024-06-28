07f4da1a14aee7df2a344a223803912e4f3fa3a6
User <User@LAPTOP-CM51QT3D>
2024-06-28 15:34:02 +0530

    First Push

---
+++ TyELF45/src/test/java/sample/Dumy2_test.java
@@ -0,0 +1,12 @@
+package sample;
+
+import org.testng.annotations.Test;
+
+public class Dumy2_test {
+	
+	@Test	
+	public void run2() {
+		System.out.println("Bye");
+	}
+
+}
---
+++ TyELF45/src/test/java/sample/Dumy_Test.java
@@ -0,0 +1,13 @@
+package sample;
+
+import org.testng.annotations.Test;
+
+public class Dumy_Test {
+	
+	
+	@Test		
+	public void run() {
+		System.out.println("Hi");
+	}
+
+}
